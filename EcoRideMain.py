4537d583035f888f8a824130bb7bfd992dbc20cf
Dhirajkumar <kumardhiraj7720@gmail.com>
2026-01-07 11:26:32 +0530

    added encapsualtion and tested

--- EcoRideMain.py
+++ EcoRideMain.py
@@ -1,4 +1,5 @@
 from Vehicle import Vehicle
+
 class EcoRideMain:
     @staticmethod
     def start():
@@ -7,12 +8,17 @@ class EcoRideMain:
         vehicle_id = input("Enter Vehicle ID: ")
         model = input("Enter Model: ")
         battery = int(input("Enter Battery Percentage: "))
+        maintenance_status = input("Enter Maintenance Status: ")
+        rental_price = int(input("Enter Rental Price: "))
 
-        vehicle = Vehicle(vehicle_id, model, battery)
-        print("Your vehicle_id-->",vehicle_id,
-        "\nYour model of car-->",model,
-        "\nYour battery percentage-->",battery,
-        "\nVehicle created successfully")
+        vehicle = Vehicle(vehicle_id, model, battery, maintenance_status, rental_price)
+
+        print("\nVehicle Created Successfully")
+        print("Vehicle ID:", vehicle.vehicle_id)
+        print("Model:", vehicle.model)
+        print("Battery:", vehicle.battery_percentage)
+        print("Maintenance:", vehicle.maintenance_status)
+        print("Rental Price:", vehicle.rental_price)
 
 if __name__ == "__main__":
     EcoRideMain.start()
